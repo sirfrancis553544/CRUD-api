b046875ebce328bb18c650ccd2d8abdba4c3a893
SirFrancis <55309898+sirfrancis553544@users.noreply.github.com>
2020-09-24 23:43:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const PORT = 5000;
 app.use(bodyParser.json());
 
 app.use("/users", usersRoutes)
-
+ 
 
 app.get("/", (req, res) => res.send("Hello from Homepage."));
 
 app.listen(PORT, () =>
   console.log(`Server running on port: http://localhost:${PORT}`)
 );
+
